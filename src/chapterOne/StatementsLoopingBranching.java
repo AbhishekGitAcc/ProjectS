a33ff3ff5af37cb996c36d17439c15d14778f07c
abhishek <abhishek@192.168.1.7>
2017-05-31 09:57:59 +0530

    first commit

---
+++ src/chapterOne/StatementsLoopingBranching.java
@@ -0,0 +1,48 @@
+package chapterOne;
+
+public class StatementsLoopingBranching {
+
+	public static void main(String[] args) {
+	
+		//Example 1 of while loop
+		
+		int x = 6 ;
+		
+		while (x > 3) {
+			
+			System.out.println("In the loop of Ex 1");
+			
+			x = x-1;
+			
+		
+		int z =27;
+		
+		while (z == 17 ) {
+			
+			System.out.println(" z is not equal to 17");
+		}
+		
+		}
+		
+		
+		// Example 2 of while loop
+		
+		int y = 1;
+		System.out.println(" Example TWO");
+		System.out.println("Before the Loop");	
+		
+		while (y < 4) {
+			System.out.println(" In the Loop of Ex 2");
+			System.out.println(" Value of x is" + y);
+			y = y+1;
+			
+		}
+
+	System.out.println("This is after the Loop");	
+	
+		
+		
+	}
+
+}
+
